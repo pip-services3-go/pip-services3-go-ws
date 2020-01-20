4df0ad16d8fcca3ae6c15adbdbb87867d1756ae5
Sergey Seroukhov <seroukhov@gmail.com>
2020-01-20 11:45:07 -0700

    Added checkout script

---
+++ checkout.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone git@github.com:pip-services3-go/pip-services3-commons-go.git
+git clone git@github.com:pip-services3-go/pip-services3-components-go.git
+git clone git@github.com:pip-services3-go/pip-services3-container-go.git
+git clone git@github.com:pip-services3-go/pip-services3-data-go.git
+git clone git@github.com:pip-services3-go/pip-services3-mongodb-go.git
+git clone git@github.com:pip-services3-go/pip-services3-rpc-go.git
+git clone git@github.com:pip-services3-go/pip-services3-messaging-go.git
