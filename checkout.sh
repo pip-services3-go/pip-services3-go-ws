99ba41dadfa63f64cb7ae45a1870a5c6ca3ef5d2
Sergey Seroukhov <seroukhov@gmail.com>
2020-02-18 15:07:10 -0700

    Added new modules

--- checkout.sh
+++ checkout.sh
@@ -7,3 +7,11 @@ git clone git@github.com:pip-services3-go/pip-services3-data-go.git
 git clone git@github.com:pip-services3-go/pip-services3-mongodb-go.git
 git clone git@github.com:pip-services3-go/pip-services3-rpc-go.git
 git clone git@github.com:pip-services3-go/pip-services3-messaging-go.git
+git clone git@github.com:pip-services3-go/pip-services3-grpc-go.git
+git clone git@github.com:pip-services3-go/pip-services3-elasticsearch-go.git
+git clone git@github.com:pip-services3-go/pip-services3-prometheus-go.git
+git clone git@github.com:pip-services3-go/pip-services3-mqtt-go.git
+git clone git@github.com:pip-services3-go/pip-services3-couchbase-go.git
+git clone git@github.com:pip-services3-go/pip-services3-memcached-go.git
+
+git clone git@github.com:pip-templates/pip-templates-microservice-go.git
